8791dd9b7162cd2f353d53db1853dbca1cef31e6
vdntruong <vdntruong@tma.com.vn>
2019-08-30 17:54:21 +0700

    c01 create pro do first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Menu from './components/menu/Menu';
+import routes from './routes';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Menu></Menu>
+
+        <div className="container">
+          <div className="row mt-5 justify-content-center">
+            <Switch>
+              {routes && routes.map((r, i) => {
+                return <Route key={i} path={r.path} exact={r.exact} component={r.main} />
+              })}
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
